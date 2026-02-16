d1880873cc36c04edbe8f6032563dcbae4ac200d
Rohan Rawat <167686138+rohanrawat10@users.noreply.github.com>
2026-02-16 23:18:31 +0530

    Clean up useUpdateLocation hook
    Refactor useUpdateLocation hook to remove commented-out code and improve clarity.

--- frontend/src/hooks/useUpdateLocation.jsx
+++ frontend/src/hooks/useUpdateLocation.jsx
@@ -1,26 +1,3 @@
-// import axios from "axios"
-// import { serverUrl } from "../config"
-// import { useEffect } from "react"
-// import { useDispatch, useSelector } from "react-redux"
-
-// export default function useUpdateLocation(){
-//     const dispactch = useDispatch();
-//     const {userData} = useSelector(state=>state.user)
-//     useEffect(()=>{
-//          const updateLocation = async(lat,lon)=>{
-
-//             const result = await axios.post(`${serverUrl}/api/user/update-location`,{lat,lon},{withCredentials:true})
-//                console.log("update location:",result.data)
-        
-//         navigator.geolocation.watchPosition((pos)=>{
-//             updateLocation(pos.coords.latitude,pos.coords.longitude)
-//         })
-//     }
-
-//     },[userData])
-// }
-
-// i
 
 
 import axios from "axios"
@@ -51,7 +28,7 @@ export default function useUpdateLocation() {
 
     const watchId = navigator.geolocation.watchPosition(
       (pos) => {
-         console.log("POSITION:", pos.coords)
+         // console.log("POSITION:", pos.coords)
         updateLocation(pos.coords.latitude, pos.coords.longitude)
       },
       (err) => console.log("geo error:", err),
